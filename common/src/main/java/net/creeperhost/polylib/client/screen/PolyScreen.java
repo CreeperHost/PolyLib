4feb4fc5ad3f0808d1b534f3a811f7ffe09a3c2c
Gigabit101 <thegigabit101@gmail.com>
2022-03-30 15:24:44 +0100

    Added Widgets for ScreenList

---
+++ common/src/main/java/net/creeperhost/polylib/client/screen/PolyScreen.java
@@ -0,0 +1,13 @@
+package net.creeperhost.polylib.client.screen;
+
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+
+//This is just a placeholder for the moment
+public class PolyScreen extends Screen
+{
+    public PolyScreen(Component component)
+    {
+        super(component);
+    }
+}
