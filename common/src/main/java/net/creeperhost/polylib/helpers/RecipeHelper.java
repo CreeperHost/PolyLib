4229a333bce6fa468cf4ce01d7d863f08ec337bf
Gigabit101 <thegigabit101@gmail.com>
2022-07-07 15:53:03 +0100

    Add RecipeHelper

---
+++ common/src/main/java/net/creeperhost/polylib/helpers/RecipeHelper.java
@@ -0,0 +1,17 @@
+package net.creeperhost.polylib.helpers;
+
+import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.level.Level;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class RecipeHelper
+{
+    public static Set<Recipe<?>> findRecipesByType(RecipeType<?> typeIn, Level world)
+    {
+        return world != null ? world.getRecipeManager().getRecipes().stream().filter(recipe -> recipe.getType() == typeIn).collect(Collectors.toSet()) : Collections.emptySet();
+    }
+}
