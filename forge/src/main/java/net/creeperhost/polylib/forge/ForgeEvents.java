332f2b707fecc6b15cf1923ce7b7109b03f03cf3
Gigabit101 <thegigabit101@gmail.com>
2022-07-14 17:28:33 +0100

    Port to forge 41.0.98

--- forge/src/main/java/net/creeperhost/polylib/forge/ForgeEvents.java
+++ forge/src/main/java/net/creeperhost/polylib/forge/ForgeEvents.java
@@ -4,7 +4,7 @@ import net.creeperhost.polylib.PolyLib;
 import net.creeperhost.polylib.events.ChunkEvents;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.chunk.LevelChunk;
-import net.minecraftforge.event.world.ChunkEvent;
+import net.minecraftforge.event.level.ChunkEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -14,12 +14,12 @@ public class ForgeEvents
     @SubscribeEvent
     public static void onChunkLoadEvent(ChunkEvent.Load event)
     {
-        ChunkEvents.CHUNK_LOAD_EVENT.invoker().onChunkLoad((Level) event.getWorld(), (LevelChunk) event.getChunk());
+        ChunkEvents.CHUNK_LOAD_EVENT.invoker().onChunkLoad((Level) event.getLevel(), (LevelChunk) event.getChunk());
     }
 
     @SubscribeEvent
     public static void onChunkUnloadEvent(ChunkEvent.Unload event)
     {
-        ChunkEvents.CHUNK_UNLOAD_EVENT.invoker().onChunkUnload((Level) event.getWorld(), (LevelChunk) event.getChunk());
+        ChunkEvents.CHUNK_UNLOAD_EVENT.invoker().onChunkUnload((Level) event.getLevel(), (LevelChunk) event.getChunk());
     }
 }
