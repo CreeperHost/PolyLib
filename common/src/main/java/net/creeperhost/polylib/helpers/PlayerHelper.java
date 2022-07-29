4f741c91685566bc859c657f91054ee3326a8d1c
Gigabit101 <thegigabit101@gmail.com>
2022-07-29 18:02:05 +0100

    Add giveItemStackToPlayer()

--- common/src/main/java/net/creeperhost/polylib/helpers/PlayerHelper.java
+++ common/src/main/java/net/creeperhost/polylib/helpers/PlayerHelper.java
@@ -1,9 +1,11 @@
 package net.creeperhost.polylib.helpers;
 
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 
 public class PlayerHelper
 {
@@ -23,5 +25,22 @@ public class PlayerHelper
     public static boolean isPlayerWearingEquipment(Player player, EquipmentSlot equipmentSlot)
     {
         return !player.getInventory().getArmor(equipmentSlot.getIndex()).isEmpty();
+    }
+
+    public static void giveItemStackToPlayer(Player player, ItemStack itemStack)
+    {
+        if(player == null) return;
+        if(itemStack.isEmpty()) return;
+        if(player.level == null) return;
+
+        //Try and add the itemStack to the player
+        boolean added = player.addItem(itemStack);
+        //If it fails due to the players inventory being full add an itementity of the stack below the player
+        if(!added)
+        {
+            Level level = player.getLevel();
+            ItemEntity itemEntity = new ItemEntity(level, player.blockPosition().getX(), player.blockPosition().getY(), player.blockPosition().getZ(), itemStack);
+            level.addFreshEntity(itemEntity);
+        }
     }
 }
