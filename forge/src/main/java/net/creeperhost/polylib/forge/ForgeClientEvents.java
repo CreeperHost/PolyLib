d3c474a51d15b4b22f7444b275ab8e2d296323e3
Gigabit101 <travis_n@creeperhost.net>
2023-06-10 02:06:47 +0100

    Fix renderLastEvent Forge

--- forge/src/main/java/net/creeperhost/polylib/forge/ForgeClientEvents.java
+++ forge/src/main/java/net/creeperhost/polylib/forge/ForgeClientEvents.java
@@ -3,17 +3,17 @@ package net.creeperhost.polylib.forge;
 import net.creeperhost.polylib.PolyLib;
 import net.creeperhost.polylib.events.ClientRenderEvents;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RenderLevelStageEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = PolyLib.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class ForgeClientEvents
 {
-    //TODO
-//    @SuppressWarnings("removal")
-//    @SubscribeEvent
-//    static void renderWorldLastEvent(RenderLevelLastEvent event)
-//    {
-//        ClientRenderEvents.LAST.invoker().onRenderLastEvent(event.getPoseStack());
-//    }
+    @SubscribeEvent
+    static void renderWorldLastEvent(RenderLevelStageEvent event)
+    {
+        if(event.getStage() == RenderLevelStageEvent.Stage.AFTER_SKY)
+            ClientRenderEvents.LAST.invoker().onRenderLastEvent(event.getPoseStack());
+    }
 }
