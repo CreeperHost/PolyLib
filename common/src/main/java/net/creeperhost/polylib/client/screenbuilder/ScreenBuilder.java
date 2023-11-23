591d5c8896183a7e7d37ee51e11862aca3e44993
Gigabit101 <thegigabit101@gmail.com>
2023-11-23 13:29:38 +0000

    Deprecated ScreenBuilder for ModularGui

--- common/src/main/java/net/creeperhost/polylib/client/screenbuilder/ScreenBuilder.java
+++ common/src/main/java/net/creeperhost/polylib/client/screenbuilder/ScreenBuilder.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Deprecated(forRemoval = true)
 public class ScreenBuilder
 {
     public static final ResourceLocation DEFAULT_RESOURCE_LOCATION = new ResourceLocation(PolyLib.MOD_ID,
