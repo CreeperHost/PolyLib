a66cd2c19eeb3cc251aca4452e54989eb20a895a
Gigabit101 <thegigabit101@gmail.com>
2023-11-24 15:37:11 +0000

    deprecate old spawn registry

--- common/src/main/java/net/creeperhost/polylib/mixins/InvokerSpawnPlacements.java
+++ common/src/main/java/net/creeperhost/polylib/mixins/InvokerSpawnPlacements.java
@@ -1,17 +1,17 @@
-package net.creeperhost.polylib.mixins;
-
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.SpawnPlacements;
-import net.minecraft.world.level.levelgen.Heightmap;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Invoker;
-
-@Mixin(SpawnPlacements.class)
-public interface InvokerSpawnPlacements
-{
-    @Invoker
-    static <T extends Mob> void callRegister(EntityType<?> entityType, SpawnPlacements.Type type, Heightmap.Types types, SpawnPlacements.SpawnPredicate<?> spawnPredicate)
-    {
-    }
-}
+//package net.creeperhost.polylib.mixins;
+//
+//import net.minecraft.world.entity.EntityType;
+//import net.minecraft.world.entity.Mob;
+//import net.minecraft.world.entity.SpawnPlacements;
+//import net.minecraft.world.level.levelgen.Heightmap;
+//import org.spongepowered.asm.mixin.Mixin;
+//import org.spongepowered.asm.mixin.gen.Invoker;
+//
+//@Mixin(SpawnPlacements.class)
+//public interface InvokerSpawnPlacements
+//{
+//    @Invoker
+//    static <T extends Mob> void callRegister(EntityType<?> entityType, SpawnPlacements.Type type, Heightmap.Types types, SpawnPlacements.SpawnPredicate<?> spawnPredicate)
+//    {
+//    }
+//}
--- common/src/main/java/net/creeperhost/polylib/registry/SpawnRegistry.java
+++ common/src/main/java/net/creeperhost/polylib/registry/SpawnRegistry.java
@@ -1,16 +1,16 @@
 package net.creeperhost.polylib.registry;
 
-import net.creeperhost.polylib.mixins.InvokerSpawnPlacements;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.SpawnPlacements;
 import net.minecraft.world.level.levelgen.Heightmap;
 
 import java.util.function.Supplier;
 
+@Deprecated(forRemoval = true)
 public class SpawnRegistry
 {
     public static void registerSpawnPlacement(Supplier<EntityType<?>> entityType, SpawnPlacements.Type type, Heightmap.Types types, SpawnPlacements.SpawnPredicate<?> spawnPredicate)
     {
-        InvokerSpawnPlacements.callRegister(entityType.get(), type, types, spawnPredicate);
+//        InvokerSpawnPlacements.callRegister(entityType.get(), type, types, spawnPredicate);
     }
 }
