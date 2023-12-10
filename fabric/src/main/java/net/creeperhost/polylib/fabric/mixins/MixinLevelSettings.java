94b8f5a58ee49c4481a9c5cc777ac73eb83fec87
Gigabit101 <thegigabit101@gmail.com>
2023-12-10 22:43:22 +0000

    Remove LevelData mixins from common as they fail to remap on NeoForge

---
+++ fabric/src/main/java/net/creeperhost/polylib/fabric/mixins/MixinLevelSettings.java
@@ -0,0 +1,25 @@
+package net.creeperhost.polylib.fabric.mixins;
+
+import net.creeperhost.polylib.events.DifficultyChangedEvent;
+import net.minecraft.world.Difficulty;
+import net.minecraft.world.level.LevelSettings;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LevelSettings.class)
+public class MixinLevelSettings
+{
+    @Shadow
+    @Final
+    private Difficulty difficulty;
+
+    @Inject(method = "withDifficulty", at = @At("HEAD"))
+    public void setDifficulty(Difficulty newDifficulty, CallbackInfoReturnable<LevelSettings> cir)
+    {
+        DifficultyChangedEvent.DIFFICULTY_CHANGED.invoker().onDifficultyChanged(newDifficulty, difficulty);
+    }
+}
---
+++ neoforge/src/main/java/net/creeperhost/polylib/forge/mixins/MixinClientLevelData.java
@@ -0,0 +1,23 @@
+package net.creeperhost.polylib.forge.mixins;
+
+import net.creeperhost.polylib.events.DifficultyChangedEvent;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.world.Difficulty;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientLevel.ClientLevelData.class)
+public abstract class MixinClientLevelData
+{
+    @Shadow
+    public abstract Difficulty getDifficulty();
+
+    @Inject(method = "setDifficulty", at = @At("HEAD"))
+    public void setDifficulty(Difficulty newDifficulty, CallbackInfo ci)
+    {
+        DifficultyChangedEvent.DIFFICULTY_CHANGED.invoker().onDifficultyChanged(newDifficulty, getDifficulty());
+    }
+}
