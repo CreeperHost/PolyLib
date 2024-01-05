77e4a976c7a26762c7eddcfe8fd4cdad30fa3f71
brandon3055 <brandon4261@gmail.com>
2024-01-05 15:01:28 +1100

    Fixed JEI plugin not working on fabric

--- common/src/main/java/net/creeperhost/polylib/compat/jei/PolyJEIPlugin.java
+++ common/src/main/java/net/creeperhost/polylib/compat/jei/PolyJEIPlugin.java
@@ -34,6 +34,5 @@ public class PolyJEIPlugin implements IModPlugin {
                 return containerScreen.getModularGui().getJeiExclusions().map(e -> e.getRectangle().toRect2i()).toList();
             }
         });
-
     }
 }
