b99a386afa295173c5ea10bb6cb0d0d8e1a49933
brandon3055 <brandon4261@gmail.com>
2024-03-18 15:00:05 +1100

    Fixed fluid and energy item handling on fabric.

--- forge/src/main/java/net/creeperhost/polylib/forge/inventory/power/ForgeEnergyManager.java
+++ forge/src/main/java/net/creeperhost/polylib/forge/inventory/power/ForgeEnergyManager.java
@@ -2,6 +2,7 @@ package net.creeperhost.polylib.forge.inventory.power;
 
 import net.creeperhost.polylib.inventory.power.EnergyManager;
 import net.creeperhost.polylib.inventory.power.IPolyEnergyStorage;
+import net.creeperhost.polylib.inventory.power.IPolyEnergyStorageItem;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -25,10 +26,10 @@ public class ForgeEnergyManager implements EnergyManager {
     }
 
     @Override
-    public @Nullable IPolyEnergyStorage getItemEnergyStorage(ItemStack stack) {
+    public @Nullable IPolyEnergyStorageItem getItemEnergyStorage(ItemStack stack) {
         LazyOptional<IEnergyStorage> optional = stack.getCapability(ForgeCapabilities.ENERGY);
         if (optional.isPresent()) {
-            return new ForgePolyEnergyWrapper(optional.orElseThrow(IllegalStateException::new));
+            return new ForgePolyEnergyWrapper(optional.orElseThrow(IllegalStateException::new), stack);
         }
         return null;
     }
