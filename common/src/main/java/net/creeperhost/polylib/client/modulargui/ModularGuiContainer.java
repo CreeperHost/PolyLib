8898d53816d9991345a49edac7dc7fddfd17ae9a
brandon3055 <brandon4261@gmail.com>
2024-07-08 13:20:29 +1000

    Fix broken Screen close event

--- common/src/main/java/net/creeperhost/polylib/client/modulargui/ModularGuiContainer.java
+++ common/src/main/java/net/creeperhost/polylib/client/modulargui/ModularGuiContainer.java
@@ -145,8 +145,8 @@ public class ModularGuiContainer<T extends AbstractContainerMenu> extends Abstra
     }
 
     @Override
-    public void onClose() {
-        super.onClose();
+    public void removed() {
+        super.removed();
         modularGui.onGuiClose();
     }
 
