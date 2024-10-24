3cef47b259162c06bf990816822041d354793075
Gigabit101 <thegigabit101@gmail.com>
2024-10-24 13:41:26 +0100

    Set this to true as we can do this for the rest of the mods during ports

--- common/src/main/java/net/creeperhost/polylib/config/PolyConfig.java
+++ common/src/main/java/net/creeperhost/polylib/config/PolyConfig.java
@@ -7,8 +7,7 @@ import blue.endless.jankson.Comment;
  */
 public class PolyConfig extends ConfigData {
 
-    //TODO, Later, Once mods have implemented the PolyLib.initPolyItemData() call, this can be changed to true by default.
     @Comment ("Allows poly lib to run as a server side only support. Without this, non-vanilla clients without polylib may not be able to connect.")
-    public boolean serverOnlySupport = false;
+    public boolean serverOnlySupport = true;
 
 }
