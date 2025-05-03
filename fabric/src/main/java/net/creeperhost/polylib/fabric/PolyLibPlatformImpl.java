d1337fff12d0a251cc80477372a9dadf586b31d2
brandon3055 <brandon4261@gmail.com>
2025-05-03 13:17:02 +1000

    Revert "Start removing TeamRebornEnergy as a hard dep"
    This reverts commit 7e2e9c511dfd773f0b4fff905707fe173be6274a.
    Reverting this only because I already had this fix written for 1.21/1.21.4, so i'm reverting this cherry-picking my fix for consistency between branches.

--- fabric/src/main/java/net/creeperhost/polylib/fabric/PolyLibPlatformImpl.java
+++ fabric/src/main/java/net/creeperhost/polylib/fabric/PolyLibPlatformImpl.java
@@ -1,7 +1,5 @@
 package net.creeperhost.polylib.fabric;
 
-import dev.architectury.platform.Platform;
-import net.creeperhost.polylib.PolyLib;
 import net.creeperhost.polylib.fabric.inventory.fluid.FabricFluidManager;
 import net.creeperhost.polylib.fabric.inventory.power.FabricEnergyManager;
 import net.creeperhost.polylib.inventory.fluid.FluidManager;
@@ -9,6 +7,7 @@ import net.creeperhost.polylib.inventory.power.EnergyManager;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import team.reborn.energy.api.EnergyStorage;
 
 import java.nio.file.Path;
 
@@ -24,26 +23,14 @@ public class PolyLibPlatformImpl
 
     public static boolean isEnergyContainer(BlockEntity block, Direction direction)
     {
-        if(Platform.isModLoaded("team_reborn_energy"))
-        {
-            return TeamRebornEnergyCompat.isEnergyContainer(block, direction);
-        }
-        PolyLib.LOGGER.info("team_reborn_energy is not loaded, Power systems that use polylib will not work");
-        return false;
+        return EnergyStorage.SIDED.find(block.getLevel(), block.getBlockPos(), direction) != null;
     }
 
     public static FluidManager getFluidManager() {
-        if(Platform.isModLoaded("team_reborn_energy"))
-        {
-            return FLUID_MANAGER;
-        }
-        return null;
+        return FLUID_MANAGER;
     }
 
     public static EnergyManager getEnergyManager() {
-        if(Platform.isModLoaded("team_reborn_energy")) {
-            return ENERGY_MANAGER;
-        }
-        return null;
+        return ENERGY_MANAGER;
     }
 }
