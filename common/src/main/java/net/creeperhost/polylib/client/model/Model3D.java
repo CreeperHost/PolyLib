320eaec9d303dad79116d2cda62a39e9e3a1daf4
Gigabit101 <thegigabit101@gmail.com>
2026-04-22 20:29:08 +0100

    Add Back Model3D

---
+++ common/src/main/java/net/creeperhost/polylib/client/model/Model3D.java
@@ -0,0 +1,49 @@
+package net.creeperhost.polylib.client.model;
+
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.core.Direction;
+
+import java.util.Arrays;
+
+public class Model3D
+{
+    public double minX, minY, minZ;
+    public double maxX, maxY, maxZ;
+    public TextureAtlasSprite[] textures = new TextureAtlasSprite[6];
+    public boolean[] renderSides = new boolean[]{true, true, true, true, true, true, false};
+
+    public final void setBlockBounds(double xNeg, double yNeg, double zNeg, double xPos, double yPos, double zPos)
+    {
+        minX = xNeg;
+        minY = yNeg;
+        minZ = zNeg;
+        maxX = xPos;
+        maxY = yPos;
+        maxZ = zPos;
+    }
+
+    public double sizeX()
+    {
+        return maxX - minX;
+    }
+
+    public double sizeY()
+    {
+        return maxY - minY;
+    }
+
+    public double sizeZ()
+    {
+        return maxZ - minZ;
+    }
+
+    public boolean shouldSideRender(Direction side)
+    {
+        return renderSides[side.ordinal()];
+    }
+
+    public void setTexture(TextureAtlasSprite tex)
+    {
+        Arrays.fill(textures, tex);
+    }
+}
