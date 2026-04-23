0d2205ceb9da088bdf86e12085afd97be6f475c1
Mattabase <mattabase@users.noreply.github.com>
2026-04-23 14:20:48 -0700

    feat(registry): add registerItem/registerBlock helpers that inject registry key into Properties.setId()

--- testmod-common/src/main/java/net/creeperhost/testmod/init/TestBlocks.java
+++ testmod-common/src/main/java/net/creeperhost/testmod/init/TestBlocks.java
@@ -3,10 +3,7 @@ package net.creeperhost.testmod.init;
 import net.creeperhost.polylib.registry.PolyRegistry;
 import net.creeperhost.testmod.TestModCommon;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.Identifier;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockBehaviour;
 
 import java.util.function.Supplier;
 
@@ -14,7 +11,7 @@ public class TestBlocks {
 
     public static final PolyRegistry<Block> BLOCKS = PolyRegistry.create(Registries.BLOCK, TestModCommon.MOD_ID);
 
-    public static final Supplier<Block> TEST_BLOCK = BLOCKS.register("test_block", "Test Block", () -> new Block(BlockBehaviour.Properties.of().setId(ResourceKey.create(Registries.BLOCK, Identifier.fromNamespaceAndPath(TestModCommon.MOD_ID, "test_block")))));
+    public static final Supplier<Block> TEST_BLOCK = BLOCKS.registerBlock("test_block", "Test Block", Block::new);
 
     public static void init() {
         BLOCKS.init();
