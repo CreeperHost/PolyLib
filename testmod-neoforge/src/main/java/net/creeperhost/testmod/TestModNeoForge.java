564df7a60cf652bca55b0bd8590ebe1d6904b5f0
Mattabase <mattabase@users.noreply.github.com>
2026-04-23 12:51:10 -0700

    feat: migrate testmod to PolyRegistry API, remove LazyItem/LazyBlock usage

--- testmod-neoforge/src/main/java/net/creeperhost/testmod/TestModNeoForge.java
+++ testmod-neoforge/src/main/java/net/creeperhost/testmod/TestModNeoForge.java
@@ -1,11 +1,14 @@
 package net.creeperhost.testmod;
 
+import net.creeperhost.polylib.neoforge.registry.NeoPolyRegistry;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 
 @Mod("testmod")
 public class TestModNeoForge
 {
-    public TestModNeoForge() {
+    public TestModNeoForge(IEventBus bus) {
         TestModCommon.init();
+        NeoPolyRegistry.registerToBus(bus);
     }
 }
