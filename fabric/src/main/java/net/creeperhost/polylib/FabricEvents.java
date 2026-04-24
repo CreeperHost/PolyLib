ae9cd4dc2321113f1eb990e16c15d85bd5fcdbfd
Gigabit101 <thegigabit101@gmail.com>
2026-04-24 14:46:15 +0100

    Replace FabricLevelSaveMixin with Fabric Event

--- fabric/src/main/java/net/creeperhost/polylib/FabricEvents.java
+++ fabric/src/main/java/net/creeperhost/polylib/FabricEvents.java
@@ -1,7 +1,12 @@
 package net.creeperhost.polylib;
 
+import net.creeperhost.polylib.event.events.server.PolyLevelEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+
 public class FabricEvents {
     public static void init(){
-
+        ServerLifecycleEvents.AFTER_SAVE.register((server, flush, force) -> {
+            PolyLevelEvents.LEVEL_SAVE.invoker().onSave(server.overworld());
+        });
     }
 }
