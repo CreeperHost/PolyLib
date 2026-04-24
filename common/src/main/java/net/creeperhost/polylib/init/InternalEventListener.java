27197973aaacc42f13511328e5fa2d0aa5fb3d04
Gigabit101 <thegigabit101@gmail.com>
2026-04-24 14:23:50 +0100

    Use events for Multiblocks

--- common/src/main/java/net/creeperhost/polylib/init/InternalEventListener.java
+++ common/src/main/java/net/creeperhost/polylib/init/InternalEventListener.java
@@ -1,9 +1,16 @@
 package net.creeperhost.polylib.init;
 
+import net.creeperhost.polylib.event.events.server.PolyChunkEvents;
+import net.creeperhost.polylib.event.events.server.PolyLevelEvents;
+import net.creeperhost.polylib.event.events.server.PolyServerTickEvents;
+import net.creeperhost.polylib.mulitblock.MultiblockRegistry;
+
 public class InternalEventListener
 {
     public static void init()
     {
-
+        PolyChunkEvents.CHUNK_LOAD.register(MultiblockRegistry::onChunkLoaded);
+        PolyLevelEvents.LEVEL_LOAD.register(MultiblockRegistry::onWorldUnloaded);
+        PolyServerTickEvents.LEVEL_TICK_START.register(MultiblockRegistry::tickStart);
     }
 }
