0fa1b0bfa2dbdd18ca5af589290a89eb280ed9ee
Aaron Mills <aaron_m@creeperhost.net>
2026-08-07 12:20:08 +0100

    Avoid loading client services on dedicated servers

--- fabric/src/main/java/net/creeperhost/polylib/platform/FabricNetworkHelper.java
+++ fabric/src/main/java/net/creeperhost/polylib/platform/FabricNetworkHelper.java
@@ -6,19 +6,15 @@ import net.creeperhost.polylib.accessibility.AccessibilityPrefsManager;
 import net.creeperhost.polylib.network.PolyLibNetwork;
 import net.creeperhost.polylib.network.packets.*;
 import net.creeperhost.polylib.platform.services.INetworkHelper;
-import net.creeperhost.polylib.player.serverdata.PlayerServerDataClientCache;
 import net.creeperhost.polylib.player.serverdata.SyncPlayerServerDataS2CPayload;
-import net.creeperhost.polylib.player.settings.PlayerClientSettingsClientCache;
 import net.creeperhost.polylib.player.settings.PlayerClientSettingsManager;
 import net.creeperhost.polylib.player.settings.PlayerClientSettingSyncS2CPayload;
 import net.creeperhost.polylib.player.settings.UpdatePlayerClientSettingC2SPayload;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.player.Player;
 
 import java.util.List;
 
@@ -70,33 +66,13 @@ public class FabricNetworkHelper implements INetworkHelper
     @Override
     public void initClient()
     {
-        ClientPlayNetworking.registerGlobalReceiver(ContainerClientPayload.TYPE, (payload, context) ->
-        {
-            Player player = context.player();
-            RegistryFriendlyByteBuf buf = new RegistryFriendlyByteBuf(Unpooled.wrappedBuffer(payload.data()), player.registryAccess());
-            context.client().execute(() -> PolyLibNetwork.handleContainerFromServer(player, buf));
-        });
-        ClientPlayNetworking.registerGlobalReceiver(TileDataClientPayload.TYPE, (payload, context) ->
-        {
-            Player player = context.player();
-            RegistryFriendlyByteBuf buf = new RegistryFriendlyByteBuf(Unpooled.wrappedBuffer(payload.data()), player.registryAccess());
-            context.client().execute(() -> PolyLibNetwork.handleTileDataValueFromServer(player, buf));
-        });
-        ClientPlayNetworking.registerGlobalReceiver(PlayerClientSettingSyncS2CPayload.TYPE, (payload, context) ->
-                context.client().execute(() ->
-                        PlayerClientSettingsClientCache.receive(payload.playerUUID(), payload.typeId(), payload.data())));
-        ClientPlayNetworking.registerGlobalReceiver(SyncPlayerServerDataS2CPayload.TYPE, (payload, context) ->
-                context.client().execute(() ->
-                        PlayerServerDataClientCache.receive(payload.typeId(), payload.data())));
+        FabricClientNetworkAccess.init();
     }
 
     @Override
     public void sendToServer(CustomPacketPayload payload)
     {
-        if (ClientPlayNetworking.canSend(payload.type()))
-        {
-            ClientPlayNetworking.send(payload);
-        }
+        FabricClientNetworkAccess.send(payload);
     }
 
     @Override
